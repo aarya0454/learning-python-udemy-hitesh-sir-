d01cb2952706b8b1f525d654a7766df97d673f26
lakshya <developer.aary@gmail.com>
2025-08-09 03:05:22 +0530

    in between the project to develop a file organiser

---
+++ python_projects/file_organiser.py
@@ -0,0 +1,19 @@
+import os 
+import shutil
+extension_map = {
+    "PDFs":['.pdf'],
+    "Images":['.png','.jpeg','.jpg'],
+    "TextFiles":['.txt'],
+}
+
+def get_destination_folder(filename):
+    ext = os.path.splitext(filename)[1].lower()
+    for folder,extensions in extension_map:
+        if ext in extensions:
+            return folder
+        return "others"
+def sort_files(folder_path):
+    for file in os.listdir(folder_path):
+        full_path = os.path.join(folder_path,file)
+        if os.path.isfile(full_path):
+            get_destination_folder(file)
